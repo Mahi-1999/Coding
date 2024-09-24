e3108533ea75dbaebaab291f13870b58f6fa4fcb
Maheshvari keshari <mkeshari@sgintl.com>
2024-09-24 09:01:46 +0530

    Removed Spring boot

---
+++ src/main/java/com/coding/For_loop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coding;
+
+/**
+ *
+ * @author maheshvari.keshari
+ */
+public class For_loop {
+       public static void main(String args[]){
+    int a = 10;
+    int sum = 0;
+    for (int i = 0; i<=a; i++){
+    sum = sum+i;
+}
+    System.out.print("Sum of natural No. " +sum);
+}
+}
